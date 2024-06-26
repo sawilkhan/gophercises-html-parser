fe0e6ae524a44e41619a2cf312b4a54bfa423143
sawilkhan <68275664+sawilkhan@users.noreply.github.com>
2024-06-26 14:01:18 +0530

    fixed edge cases

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/sawilkhan/gophercises-html-parser
 
 go 1.22.2
 
-require golang.org/x/net v0.26.0 // indirect
+require golang.org/x/net v0.26.0
--- parse.go
+++ parse.go
@@ -7,26 +7,29 @@ import (
 	"golang.org/x/net/html"
 )
 
+// Link represents a link (<a href="...">) in an HTML
+// document.
 type Link struct {
 	Href string
 	Text string
 }
 
-func Parse(r io.Reader) ([]Link, error){
+// Parse will take in an HTML document and will return a
+// slice of links parsed from it.
+func Parse(r io.Reader) ([]Link, error) {
 	doc, err := html.Parse(r)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	nodes := linkNodes(doc)
 	var links []Link
-	for _, node := range nodes{
-		links = append(links, build(node))
+	for _, node := range nodes {
+		links = append(links, buildLink(node))
 	}
-
 	return links, nil
 }
 
-func build(n *html.Node) Link {
+func buildLink(n *html.Node) Link {
 	var ret Link
 	for _, attr := range n.Attr {
 		if attr.Key == "href" {
@@ -38,29 +41,27 @@ func build(n *html.Node) Link {
 	return ret
 }
 
-func text(n *html.Node) string{
-	if n.Type == html.TextNode{
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
 		return n.Data
 	}
-	if n.Type != html.ElementNode{
+	if n.Type != html.ElementNode {
 		return ""
 	}
 	var ret string
-	for c := n.FirstChild; c != nil; c = c.NextSibling{
-		ret += text(c) + " "
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c)
 	}
 	return strings.Join(strings.Fields(ret), " ")
 }
 
-
-func linkNodes(n *html.Node) []*html.Node{
+func linkNodes(n *html.Node) []*html.Node {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		return []*html.Node{n}
 	}
 	var ret []*html.Node
-	for c := n.FirstChild; c != nil; c = c.NextSibling{
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		ret = append(ret, linkNodes(c)...)
 	}
-
 	return ret
 }
